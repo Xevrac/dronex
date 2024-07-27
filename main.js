efd01048e0e78801ac496205d4a8a58f311d152d
xevrac <xevrac@gmail.com>
2024-07-28 01:19:16 +1000

    Revert "Adds updater function"
    This reverts commit 1c87fd6feb4707279c4db54335f17e5776efdf46.

--- main.js
+++ main.js
@@ -1,40 +1,9 @@
-const { app, BrowserWindow, ipcMain, autoUpdater, dialog } = require('electron/main');
+const { app, BrowserWindow, ipcMain } = require('electron/main');
 const dotenv = require('dotenv');
 const path = require('node:path');
-const { updateElectronApp, UpdateSourceType } = require('update-electron-app')
 
 // Fetch env var from .env
 dotenv.config();
-
-// Check for updates on launch and intervals
-if (process.env.NODE_ENV === 'dev') { updateElectronApp({
-  repo: 'Xevrac/dronex',
-  updateInterval: '1 hour',
-  notifyUser: true,
-});
-} 
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: 'info',
-    buttons: ['Restart', 'Later'],
-    title: 'Application Update',
-    message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail:
-      'A new version has been downloaded. Restart the application to apply the updates.'
-  }
-
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) autoUpdater.quitAndInstall()
-  })
-})
-
-autoUpdater.on('error', (message) => {
-  console.error('There was a problem updating the application')
-  console.error(message)
-})
-
-/////////////////////////////
 
 const createWindow = () => {
   const win = new BrowserWindow({
